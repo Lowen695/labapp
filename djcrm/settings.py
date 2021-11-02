6175c7799dc0b733e497acc768bf1ca896c22206
Fahui Liu <fahuiliu@FahuideMBP.keinstruments.local>
2021-11-02 15:53:01 +0100

    tweaked app for heroku

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+
+
+import dj_database_url
 import os
 import django_heroku
-import dj_database_url
-from python-decouple import config
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -52,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middeleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'djcrm.urls'
